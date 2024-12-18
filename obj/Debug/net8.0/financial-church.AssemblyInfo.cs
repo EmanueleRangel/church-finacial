564856d805034bd226bf56f0c1367debfc9c8469
EmanueleRangel <emanuele.rangel52@gmail.com>
2024-12-18 09:04:54 -0300

    initial changes

--- obj/Debug/net8.0/financial-church.AssemblyInfo.cs
+++ obj/Debug/net8.0/financial-church.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("financial-church")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88854bafa1f2cdfaa50b1d1f0b25b7d24dd558c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("financial-church")]
 [assembly: System.Reflection.AssemblyTitleAttribute("financial-church")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
