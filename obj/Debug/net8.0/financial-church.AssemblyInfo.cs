f560841747ccc721238480498ab5f01574dd43d8
EmanueleRangel <emanuele.rangel52@gmail.com>
2025-01-22 10:37:24 -0300

    update readme file type

--- obj/Debug/net8.0/financial-church.AssemblyInfo.cs
+++ obj/Debug/net8.0/financial-church.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("financial-church")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46fded57bb3deebb72e6896b43c016644e59ca9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2b24210efaaaf6ce5f28a9828de391d0bd905c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("financial-church")]
 [assembly: System.Reflection.AssemblyTitleAttribute("financial-church")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
